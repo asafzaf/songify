2e9fe6efce68811f923e36001fb3b8c51a43b458
asafzaf <104023823+asafzaf@users.noreply.github.com>
2022-04-20 00:12:57 +0300

    Create album.c

---
+++ album.c
@@ -0,0 +1,23 @@
+#include "album.h"
+#include "songify.h"
+#include "song.h"
+
+Album *addSong(Songify *artists, Song *newSong, Album *TheAlbum, Artist *TheArtist)
+{
+
+    return artists;
+}
+
+int numOfsongs(Songify *artists, Album *TheAlbum)
+{
+    int counter = 0;
+
+    return counter;
+}
+
+int lenOfalbum(Songify *artists, Album *TheAlbum, Artist *TheArtist)
+{
+    int sec_count = 0, min_count = 0, hour_count = 0;
+
+    
+}
